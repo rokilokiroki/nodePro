196cf53b000c4724182c6d73b4ed592beba4b696
yamamoto hiroki <h_yamamoto@sports-f.co.jp>
2020-11-02 18:12:52 +0900

    [change] jenkinsファイルhennkou

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-cd /var/lib/jenkins/workspace/recieveNotificationFromJob/
+git clone https://github.com/rokilokiroki/nodePro.git
+cd nodePro
+npm install
 mocha formTest.js --timeout 10000 --reporter xunit > xunit.xml
+
+
+
+
+# cd /var/lib/jenkins/workspace/recieveNotificationFromJob/
+# mocha formTest.js --timeout 10000 --reporter xunit > xunit.xml
