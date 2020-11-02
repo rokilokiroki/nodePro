5a3d9ce2f07f8b3c0d7a9442b28c2850027ba94a
yamamoto hiroki <h_yamamoto@sports-f.co.jp>
2020-11-02 18:17:26 +0900

    [change] jenkins変更

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 git clone https://github.com/rokilokiroki/nodePro.git
+echo 'cloning success!'
 cd nodePro
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+echo $SCRIPT_DIR
 npm install
 mocha formTest.js --timeout 10000 --reporter xunit > xunit.xml
 
