320d02765179961b883fd0bcd02ce9adb5225011
yamamoto hiroki <h_yamamoto@sports-f.co.jp>
2020-11-02 18:49:46 +0900

    [change] test

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
+
+cd nodePro
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+echo $SCRIPT_DIR
+
+npm install
 mocha formTest.js --timeout 10000 --reporter xunit > xunit.xml
-
-
-
-
-# cd /var/lib/jenkins/workspace/recieveNotificationFromJob/
-# mocha formTest.js --timeout 10000 --reporter xunit > xunit.xml
