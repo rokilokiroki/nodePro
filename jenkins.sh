a3ab954785f46bdd20ee06b80aceaa54067a845b
yamamoto hiroki <h_yamamoto@sports-f.co.jp>
2020-11-08 01:12:30 +0900

    test

--- jenkins.sh
+++ jenkins.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-cd nodePro
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-echo $SCRIPT_DIR
+# cd nodePro
+# SCRIPT_DIR=$(cd $(dirname $0); pwd)
+# echo $SCRIPT_DIR
 
-git config --global user.name "test"
-git config --global user.email "xxxxxx@gmail.com"
-git init
-git fetch origin
-git reset --hard origin/${main}
-# git pull origin ${main}
-echo 'deployment success!'
+# git config --global user.name "test"
+# git config --global user.email "xxxxxx@gmail.com"
+# git init
+# git fetch origin
+# git reset --hard origin/${main}
+# # git pull origin ${main}
+# echo 'deployment success!'
 
-npm install
-mocha formTest.js --timeout 10000 --reporter xunit > xunit.xml
+# npm install
+# mocha formTest.js --timeout 10000 --reporter xunit > xunit.xml
